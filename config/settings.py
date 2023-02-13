978d76bfc1d45db25c700a4653c30d0c25ec3082
y-dai20 <funkymonster20@gmail.com>
2023-02-13 18:01:54 +0900

    fix static path

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'my_static'),
 )
 
 # Default primary key field type
